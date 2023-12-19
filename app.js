a9db17b02a4495ec645028eca2ca6b682d9ce7f9
richardcodez <105194810+richardcodez@users.noreply.github.com>
2023-12-19 22:00:54 +0200

    ejs snippet code

--- app.js
+++ app.js
@@ -13,29 +13,25 @@ app.set('view engine', 'ejs');
 // home page
 app.get('/', (req, res) => {
     
-    // res.send('<p>Home page</p>');
-    console.log("----path----)");
-    console.log(__dirname);
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index', { title: 'Home' });
 
 })
 
 // about page
 app.get('/about', (req, res) => {
     
-    // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 
-})
+});
 
-// Redirecting
-app.use('/about-us', (req, res) => {
-    res.redirect('/about');
-})
+// create blog
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+});
 
 // 404 page: it has to be the last function => at the bottom
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404');
 });
 
 // listen for requests
