20fde5f424440c8f2dc559edd16bce1314cf56aa
richardcodez <105194810+richardcodez@users.noreply.github.com>
2023-12-20 08:36:50 +0200

    dynamic content node dev

--- app.js
+++ app.js
@@ -12,26 +12,31 @@ app.set('view engine', 'ejs');
 
 // home page
 app.get('/', (req, res) => {
-    
-    res.render('index', { title: 'Home' });
+
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Some content goes right here just for testing' },
+        {title: 'MArio finds stars', snippet: 'Some content goes right here just for testing' },
+        {title: 'How to defeat bowser', snippet: 'Some content goes right here just for testing' }
+    ];
+    res.render('index', { title: 'Home', blogs });
 
 })
 
 // about page
 app.get('/about', (req, res) => {
     
-    res.render('about');
+    res.render('about', { title: 'About' });
 
 });
 
 // create blog
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create a New Blog' });
 });
 
 // 404 page: it has to be the last function => at the bottom
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404' });
 });
 
 // listen for requests
