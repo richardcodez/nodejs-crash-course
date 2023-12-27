b31ca83f31926354517a6e35a6bb8b56a43daa05
richardcodez <105194810+richardcodez@users.noreply.github.com>
2023-12-27 16:00:14 +0200

    managed to add a document to mongodb atlas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog.js');
 
 
 // express app
@@ -9,7 +10,7 @@ const app = express();
 // Connect to MongoDB
 const dbURI = 'mongodb+srv://richardcodez:lcr856040@cluster0.6xln7ks.mongodb.net/node-tuts?retryWrites=true&w=majority';
 mongoose.connect(dbURI)
-    .then((result) => { app.listen(3000) })
+    .then(() => { app.listen(3000) })
     .catch((err) => {console.log(err)});
 
 // Register view engine
@@ -25,6 +26,24 @@ app.set('view engine', 'ejs')
 // Middleware and STatic files = public files = css, images
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+// Mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about my new blog',
+        body: 'more about my new blog, the actual content of the blog'
+    });
+
+    blog.save()
+        .then( (result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
 
 // home page
 app.get('/', (req, res) => {
