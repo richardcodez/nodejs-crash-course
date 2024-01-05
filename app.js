3c753011424418e12ef5428bccf377c478beb728
richardcodez <105194810+richardcodez@users.noreply.github.com>
2024-01-05 14:03:21 +0200

    handling delete request using AJAX fetch api

--- app.js
+++ app.js
@@ -77,6 +77,16 @@ app.get('/blogs/:id', (req, res) => {
         .catch( (err) => {
             console.log(err);
         })
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch((err)=>console.log(err));
 })
 
 // create blog
